a366d7678ae6f2ef043ca3f8effbf0e3f9a4308d
Shantanu Sardal <ufdeveloper@gmail.com>
2022-08-07 13:27:35 -0400

    fix

---
+++ src/test/java/com/megshan/stravahr/StravahrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megshan.stravahr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StravahrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
